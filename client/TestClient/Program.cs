1056077e1e68606e34d3beccc8aebb1addcfd257
Renato Golia <renato.golia@studentum.se>
2018-01-19 18:03:29 +0100

    Test client

---
+++ client/TestClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HubSpot;
+using Microsoft.Extensions.Logging;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggerFactory loggerFactory = new LoggerFactory();
+
+            HubSpotAuthenticator authenticator = null;
+            IHubSpotClient hubspot = new HttpHubSpotClient(authenticator, loggerFactory.CreateLogger<HttpHubSpotClient>());
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
