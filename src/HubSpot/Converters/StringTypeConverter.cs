718a9582ce45787c013164ab723f1c0aff6d592f
Renato Golia <renato.golia@studentum.se>
2018-02-23 17:59:14 +0100

    Created more converters

---
+++ src/HubSpot/Converters/StringTypeConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace HubSpot.Converters {
+    public class StringTypeConverter : ITypeConverter
+    {
+        public bool TryConvertTo(string value, out object result)
+        {
+            result = value;
+            return true;
+        }
+
+        public bool TryConvertFrom(object value, out string result)
+        {
+            result = null;
+
+            if (value == null)
+            {
+                return true;
+            }
+
+            if (value is string str)
+            {
+                result = str;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
