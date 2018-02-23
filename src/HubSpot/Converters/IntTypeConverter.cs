3508a417486495e49d795e9c8b2fb121dc74d3aa
Renato Golia <renato.golia@studentum.se>
2018-02-23 17:59:14 +0100

    Created more converters

---
+++ src/HubSpot/Converters/IntTypeConverter.cs
@@ -0,0 +1,40 @@
+﻿namespace HubSpot.Converters {
+    public class IntTypeConverter : ITypeConverter
+    {
+        public bool TryConvertTo(string value, out object result)
+        {
+            result = default;
+
+            if (value == null)
+            {
+                return true;
+            }
+
+            if (int.TryParse(value, out var output))
+            {
+                result = output;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool TryConvertFrom(object value, out string result)
+        {
+            if (value == null)
+            {
+                result = null;
+                return true;
+            }
+
+            if (value is int num)
+            {
+                result = num.ToString("D");
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+    }
+}
---
+++ src/HubSpot/Converters/LongTypeConverter.cs
@@ -0,0 +1,40 @@
+﻿namespace HubSpot.Converters {
+    public class LongTypeConverter : ITypeConverter
+    {
+        public bool TryConvertTo(string value, out object result)
+        {
+            result = default;
+
+            if (value == null)
+            {
+                return true;
+            }
+
+            if (long.TryParse(value, out var output))
+            {
+                result = output;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool TryConvertFrom(object value, out string result)
+        {
+            if (value == null)
+            {
+                result = null;
+                return true;
+            }
+
+            if (value is long num)
+            {
+                result = num.ToString("D");
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+    }
+}
