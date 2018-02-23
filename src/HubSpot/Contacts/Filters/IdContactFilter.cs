708db06a47d8cd7b2aed254bb3909746de2f230f
Renato Golia <renato.golia@studentum.se>
2018-02-23 18:38:10 +0100

    Added Contacts API lists to connector

---
+++ src/HubSpot/Contacts/Filters/IdContactFilter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HubSpot.Model;
+using HubSpot.Model.Contacts;
+
+namespace HubSpot.Contacts.Filters {
+    public class IdContactFilter : ContactFilter
+    {
+        private readonly IReadOnlyList<long> _contactIds;
+
+        public IdContactFilter(IReadOnlyList<long> contactIds)
+        {
+            _contactIds = contactIds;
+        }
+
+        protected override async Task<(IReadOnlyList<Model.Contacts.Contact> list, bool hasMore, long? offset)> Get(IHubSpotClient client, IReadOnlyList<IProperty> propertiesToQuery, long? offset)
+        {
+            var response = await client.Contacts.GetManyByIdAsync(_contactIds, propertiesToQuery, PropertyMode.ValueOnly, FormSubmissionMode.None, false, false).ConfigureAwait(false);
+
+            return (response.Values.ToArray(), false, null);
+        }
+    }
+}
---
+++ src/HubSpot/Contacts/IContactFilter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HubSpot.Contacts.Filters;
+using HubSpot.Model;
+using HubSpotContact = HubSpot.Model.Contacts.Contact;
+
+namespace HubSpot.Contacts
+{
+    public interface IContactFilter
+    {
+        Task<IReadOnlyList<HubSpotContact>> GetContacts(IHubSpotClient client, IReadOnlyList<IProperty> propertiesToQuery);
+    }
+
+    public static class FilterContacts
+    {
+        public static IContactFilter All => new AllContactFilter();
+
+        public static IContactFilter RecentlyModified => new RecentlyUpdatedContactFilter();
+
+        public static IContactFilter RecentlyCreated => new RecentlyCreatedContactFilter();
+
+        public static IContactFilter ByEmail(params string[] emails) => new EmailContactFilter(emails);
+
+        public static IContactFilter ById(params long[] ids) => new IdContactFilter(ids);
+
+        public static IContactFilter Query(string searchQuery) => new SearchContactFilter(searchQuery);
+    }
+}
