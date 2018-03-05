1bc3068859b2d3021029f7b9cc30f26c9e0d4a1d
Renato Golia <renato.golia@studentum.se>
2018-03-05 16:21:43 +0100

    Added LookupBuilder customization for AutoFixture
    From: https://github.com/AutoFixture/AutoFixture/issues/991#issuecomment-370262186

---
+++ tests/Tests.HubSpot/LookupBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AutoFixture;
+using AutoFixture.Kernel;
+
+namespace Tests
+{
+    public class LookupBuilder : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (request is Type typeRequest
+                && typeRequest.IsGenericType
+                && typeRequest.GetGenericTypeDefinition() == typeof(ILookup<,>))
+            {
+                var args = typeRequest.GetGenericArguments();
+                return this.GetType()
+                           .GetMethod(nameof(GenerateTypedLookup), BindingFlags.Static | BindingFlags.NonPublic)
+                           .MakeGenericMethod(args)
+                           .Invoke(null, new object[] { context });
+            }
+
+            return new NoSpecimen();
+        }
+
+        private static ILookup<TKey, TValue> GenerateTypedLookup<TKey, TValue>(ISpecimenContext context)
+        {
+            return context.CreateMany<KeyValuePair<TKey, IEnumerable<TValue>>>()
+                          // Make sequence flat to later generate lookup from it.
+                          .SelectMany(kv => kv.Value.Select(v => new KeyValuePair<TKey, TValue>(kv.Key, v)))
+                          .ToLookup(kv => kv.Key, kv => kv.Value);
+        }
+    }
+}
