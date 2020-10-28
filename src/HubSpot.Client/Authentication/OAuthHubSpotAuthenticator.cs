306c3c1facd33612ee537e4443114279f93a3836
Renato Golia <renato.golia@outlook.com>
2020-10-28 18:16:58 +0100

    Updated all projects to latest version of packages

--- src/HubSpot.Client/Authentication/OAuthHubSpotAuthenticator.cs
+++ src/HubSpot.Client/Authentication/OAuthHubSpotAuthenticator.cs
@@ -5,7 +5,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
-using EMG.Common;
+using EMG.Utilities;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Linq;
 
